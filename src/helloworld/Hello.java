1f95d3343c50d226e070ae564a1951ac5b192642
Artem Gratchev <a.gratchev@sap.com>
2019-10-21 11:19:49 +0200

    Initial commit

---
+++ src/helloworld/Hello.java
@@ -0,0 +1,21 @@
+package helloworld;
+
+public class Hello {
+
+	public static int sum(int a, int b) {
+		// Dragomir
+		
+		return 0;
+	}
+	
+	
+	public static void print(int sum) {
+		// Peter
+	}
+	
+	
+	public static void main(String[] args) {
+		print(sum(2, 3));
+	}
+
+}
